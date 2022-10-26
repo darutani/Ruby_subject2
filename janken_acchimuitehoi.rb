f4503fcba239f8a4e9c243b17bd061dcda50df3b
natsuki <su811sh1@gmail.com>
2022-10-26 11:57:05 +0900

    Rubyの課題２（じゃんけんアプリ）を提出

--- janken_acchimuitehoi.rb
+++ janken_acchimuitehoi.rb
@@ -7,8 +7,9 @@ def janken        #じゃんけんのメソッド
   if user_throw == 3        #戦わないを選択した場合の処理
     puts "-------------------"
     puts "ゲームを終了します。またの挑戦をお待ちしております。"
+    return "finish"
     
-  else          #戦う選択をした場合の処理
+  elsif user_throw == 0 || user_throw == 1 || user_throw == 2          #戦う選択をした場合の処理
     choice = ["グー", "チョキ", "パー", "お疲れ様でした"]
     opponent_throw = rand(3)
     
@@ -28,14 +29,12 @@ def janken        #じゃんけんのメソッド
       puts "あなたの負けです"
       return "janken_lose"
     end
+  else            #0,1,2,3以外を選択した場合
+    puts "-------------------"
+    puts "０、１、２、３のいずれかを選択ください"
+    janken
   end  
 end  
-
-#あいこの場合にはじゃんけんを繰り返す
-janken_judge = "janken_draw"
-while janken_judge == "janken_draw"
-  janken_judge = janken
-end
 
 
 def acchimuitehoi(result)         #あっち向いてホイのメソッド
@@ -49,18 +48,15 @@ def acchimuitehoi(result)         #あっち向いてホイのメソッド
   puts "ホイ！"
   puts "-------------------"
   
- if user_direction != opponent_direction          #あっち向いてホイの決着がつかなかった場合
+ if user_direction != 0 && user_direction != 1 && user_direction != 2 && user_direction != 3          #0,1,2,3以外を選択した場合
+  puts "０、１、２、３のいずれかを選択ください"
+  return "error"
+  
+ elsif user_direction != opponent_direction          #あっち向いてホイの決着がつかなかった場合
   puts "あなた：#{choice[user_direction]}"
   puts "相手：#{choice[opponent_direction]}"
   puts "-------（ドロー）------------"
   puts "じゃんけん..."
-
-  #再度じゃんけんの処理を繰り返す
-  janken_judge = "janken_draw"
-  while janken_judge == "janken_draw"
-    janken_judge = janken
-  end
-
   return "acchimuitehoi_draw"
 
  elsif user_direction == opponent_direction && result == "janken_win"         #あっち向いてホイで勝った場合
@@ -80,9 +76,23 @@ def acchimuitehoi(result)         #あっち向いてホイのメソッド
 end
 
 
-#決着がつくまであっち向いてホイを繰り返す
+#じゃんけんとあっち向いてホイの処理実行
+janken_judge = "janken_draw"
 acchimuitehoi_judge = "acchimuitehoi_draw"
-while acchimuitehoi_judge == "acchimuitehoi_draw"
-  acchimuitehoi_judge = acchimuitehoi(janken_judge)
-end
 
+while janken_judge == "janken_draw" || acchimuitehoi_judge == "acchimuitehoi_draw"
+  janken_judge = janken         #じゃんけんの処理実行
+
+  if janken_judge == "janken_draw"         #じゃんけんがあいこの場合にはあっち向いてホイの処理に移行しないよう制御
+    next
+  elsif janken_judge == "finish"           #3(戦わない)を選択した場合処理を終了する
+    break
+  end
+
+  acchimuitehoi_judge = acchimuitehoi(janken_judge)         #あっち向いてホイの処理実行
+  if acchimuitehoi_judge == "error"                         #0,1,2,3以外を選択した場合に、再度あっち向いてホイの選択をさせる
+    while acchimuitehoi_judge == "error"
+      acchimuitehoi_judge = acchimuitehoi(janken_judge)
+    end
+  end
+end
